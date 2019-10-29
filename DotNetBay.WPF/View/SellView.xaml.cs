1767a1fe14c69f917a4bddd66267e1d39430718a
Nicolas <nicolas.odermatt@students.fhnw.ch>
2019-10-29 17:45:24 +0100

    Add SellViewModel and BaseViewModel

---
+++ DotNetBay.WPF/View/SellView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DotNetBay.WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DotNetBay.WPF.View
+{
+    /// <summary>
+    /// Interaction logic for SellView.xaml
+    /// </summary>
+    public partial class SellView : Window
+    {
+
+        SellViewModel viewModel;
+
+        public SellView()
+        {
+            InitializeComponent();
+            this.DataContext = new SellViewModel();
+        }
+    }
+}
---
+++ DotNetBay.WPF/ViewModel/ViewModelBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBay.WPF.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
